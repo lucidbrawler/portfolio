130f1240b1217bb26b341d4327bf8e9ad1d1d0a8
lucidbrawler <kennethpsinclair@yahoo.com>
2022-05-05 09:11:48 -0400

    links redirect properly

--- src/routes/resume.js
+++ src/routes/resume.js
@@ -37,13 +37,15 @@ const Resume = () => (
         </section>
         <section>
             Portfolio Links:<br/><br/><ul>
-           <li> <a href='https://www.recipelab.fun/login'>RECIPE LAB</a><br/></li>
+            <li><a target="blank" href= ' https://blissful-knuth-d7ec88.netlify.app/'>KOHILO WIND</a><br/></li>
 
-           <li> <a href=  'https://hatchetpress.net/#/'>HATCHET PRESS</a><br/></li>
+            <li> <a  target="blank" href='https://www.recipelab.fun/login'>RECIPE LAB</a><br/></li>
 
-           <li><a href= ' https://blissful-knuth-d7ec88.netlify.app/'>KOHILO WIND</a><br/></li>
+            <li> <a target="blank" href=  'https://hatchetpress.net/#/'>HATCHET PRESS</a><br/></li>
 
-           <li><a href= ' https://peaceful-carson-4994a2.netlify.app/'>SCRATCH FARMHOUSE CATERING</a><br/></li>
+            <li><a target="blank" href= ' https://blissful-knuth-d7ec88.netlify.app/'>KOHILO WIND</a><br/></li>
+
+            <li><a target="blank" href= ' https://peaceful-carson-4994a2.netlify.app/'>SCRATCH FARMHOUSE CATERING</a><br/></li>
             </ul>
         </section>
         </section>
