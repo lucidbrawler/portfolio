c8f4e0c03ceef9880d89e99e5e7b7b7011291f0a
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2025-01-11 21:53:59 -0500

    added hover link previews for website portfolio links

--- src/routes/landing.js
+++ src/routes/landing.js
@@ -4,12 +4,15 @@ import { InlineWidget } from "react-calendly";
 
 
 const Landing = () => (
-  
+  <div className="App">
+    <h1>Hatchet Tech</h1>
  
-    <div className="App">
+    <div className="Calendly" >
       <InlineWidget styles={{
-                    height: '900px'}}
+                    height: '800px'}}
                     url="https://calendly.com/hatchetpressmail/web-services-consultation" />
+    </div>
+    <h1>Hatchet Tech</h1>
     </div>
 );
     export default Landing;
--- src/routes/websites.js
+++ src/routes/websites.js
@@ -1,19 +1,30 @@
 
 import "../App.css"
+import recipelab from '../images/recipelab.png';
+import Warthog from '../images/warthognetwork.png';
+import jodygrenier from '../images/jodygrenierart.png';
+import hatchetpress from '../images/HatchetPress.png';
 const Websites = () => {
 
+
+   
     return(
 <div className="">
     These are two of my current running websites, excluding this one.<br/><br/>
-    <section>
-            Portfolio Links:<br/><br/><ul>
-           <li> <a target="blank" href='https://www.recipelab.fun/login'>RECIPE LAB</a><br/></li>
+    Portfolio Links:<br/><br/>
+    <section className="wrapper">
+            
+           <a target="blank" className="box" href='https://www.recipelab.fun/login'>RECIPE LAB</a><br/>
 
-           <li> <a target="blank" href=  'https://hatchetpress.net/#/'>HATCHET PRESS</a><br/></li>
-
+            <a target="blank" className="box2" href=  'https://hatchetpress.net/#/'>HATCHET PRESS</a><br/>
+           <a target="blank" className="box3" href=  'https://Warthog.Network'>Warthog Network</a><br/>
+            <a target="blank" className="box4" href=  'https://jodygrenier.art/#/'>Jody Grenier Photography</a><br/>
+            <img className='effect' alt="" src={recipelab}/>
+            <img className='effect2' alt="" src={hatchetpress}/>
+            <img className='effect3' alt="" src={Warthog}/>
+            
+            <img className='effect4' alt="" src={jodygrenier}/>
            
-
-            </ul>
         </section>
 </div>)
 
